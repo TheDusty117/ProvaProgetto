3bfc449348fb1bae2ac5f3462ad152c6753909e4
nicola00 <nicolajobs00@gmail.com>
2023-05-17 10:53:28 +0200

    SECONDO CHANGE NICOLA

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -420,7 +420,7 @@
         </div>
         @endif
         <h1>
-            CIAO SONO NICOLA
+            CIAO SONO NICOLA QUESTO é IL SECONDO
         </h1>
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
